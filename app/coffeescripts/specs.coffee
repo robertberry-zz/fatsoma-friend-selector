4ad3359295e386c6f5b739bc492245881fb0e9bf
Robert Berry <rjberry@gmail.com>
2012-02-10 11:28:48 +0000

    Fixing test specs too - had to move all into app folder, though.

---
+++ app/coffeescripts/specs.coffee
@@ -0,0 +1,52 @@
+require ["views", "jquery"], (views) ->
+ describe "FriendSelector", ->
+   selector = null
+   elems = null
+
+   beforeEach ->
+     # create a fake friend selector for testing on
+     elems = $('''
+              <div id="selector">
+                <input type="text" class="search" />
+                <div class="autocomplete"></div>
+                <div class="selected"></div>
+                <input type="submit" />
+              </div>
+              ''')
+
+     # add to DOM
+     $("body").append elems
+     selector = new views.FriendSelector(el: $("#selector"))
+
+   afterEach ->
+     selector = null
+     # remove from DOM
+     elems.remove()
+     elems = null
+
+   describe "search box", ->
+     search_box = null
+
+     beforeEach ->
+       search_box = selector.$(".search")
+
+     afterEach ->
+       search_box = null
+
+     it "should be initially empty", ->
+       expect(search_box.val()).toBeFalsy()
+
+     it "should update the FriendSelector search term when content changes", ->
+       for name in ["Rob", "Chris", "Paul", "James", "Aaron"]
+         search_box.val name
+         expect(search_box.val()).toEqual selector.search_term()
+
+     it "should try to render an autocomplete dropdown when input changes", ->
+       spyOn selector, "render_autocomplete"
+       # you must call delegateEvents after spying on a method that will be
+       # called via a callback as specified in Backbone.View events hash. this
+       # is to do with the the original method being wrapped in a function and
+       # that the event triggers the wrapped function, not the wrapper
+       selector.delegateEvents()
+       search_box.keypress()
+       expect(selector.render_autocomplete).toHaveBeenCalled()
---
+++ app/scripts/specs.js
@@ -0,0 +1,53 @@
+(function() {
+
+  require(["views", "jquery"], function(views) {
+    return describe("FriendSelector", function() {
+      var elems, selector;
+      selector = null;
+      elems = null;
+      beforeEach(function() {
+        elems = $('<div id="selector">\n  <input type="text" class="search" />\n  <div class="autocomplete"></div>\n  <div class="selected"></div>\n  <input type="submit" />\n</div>');
+        $("body").append(elems);
+        return selector = new views.FriendSelector({
+          el: $("#selector")
+        });
+      });
+      afterEach(function() {
+        selector = null;
+        elems.remove();
+        return elems = null;
+      });
+      return describe("search box", function() {
+        var search_box;
+        search_box = null;
+        beforeEach(function() {
+          return search_box = selector.$(".search");
+        });
+        afterEach(function() {
+          return search_box = null;
+        });
+        it("should be initially empty", function() {
+          return expect(search_box.val()).toBeFalsy();
+        });
+        it("should update the FriendSelector search term when content changes", function() {
+          var name, _i, _len, _ref, _results;
+          _ref = ["Rob", "Chris", "Paul", "James", "Aaron"];
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            name = _ref[_i];
+            search_box.val(name);
+            _results.push(expect(search_box.val()).toEqual(selector.search_term()));
+          }
+          return _results;
+        });
+        return it("should try to render an autocomplete dropdown when input changes", function() {
+          spyOn(selector, "render_autocomplete");
+          selector.delegateEvents();
+          search_box.keypress();
+          return expect(selector.render_autocomplete).toHaveBeenCalled();
+        });
+      });
+    });
+  });
+
+}).call(this);
