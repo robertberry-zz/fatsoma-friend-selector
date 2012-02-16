fb006fb2becc01d59161cd3ab1dfa0a630d3e17e
Chris Pearson <chris@uz7ho9ex.(none)>
2012-02-16 15:51:43 +0000

    fix remove selected friend tests

--- app/coffeescripts/specs.coffee
+++ app/coffeescripts/specs.coffee
@@ -204,19 +204,21 @@ require ["views", "utils", "models", "exceptions", "fixtures"], \
         first_friend = selector.friends.models[0]
         selected.collection.add first_friend
         selector.search.set_query first_friend.attributes.name
-        expect(selector.autocomplete.collection.models).not.toContain first_friend
+        expect(selector.autocomplete.collection.models).not.toContain \
+            first_friend
 
-     describe "when I press remove a selected friend", ->
-       beforeEach ->
-         first_friend = selector.friends.models[0]
-         selected.add_model first_friend
+      describe "when I press remove a selected friend", ->
+        first_friend = null;
+        
+        beforeEach ->
+          first_friend = selector.friends.models[0]
+          selected.collection.add first_friend
+          selected.items[0].remove()
 
-       it "the friend should not appear in the selected friends", ->
-         selected.items[0].remove()
-         expect(selected.collection.models).toNotContain first_friend
+        it "the friend should not appear in the selected friends", ->
+          expect(selected.collection.models).toNotContain first_friend
 
-       it "the friend should appear in the autocomplete when I search for
+        it "the friend should appear in the autocomplete when I search for
         their name", ->
-         selected.items[0].remove()
-         selector.search.set_query first_friend.attributes.name
-         expect(selector.autocomplete.collection.models).toContain first_friend
+          selector.search.set_query first_friend.attributes.name
+          expect(selector.autocomplete.collection.models).toContain first_friend
--- app/scripts/specs.js
+++ app/scripts/specs.js
@@ -206,7 +206,7 @@
           });
         });
       });
-      describe("selected list", function() {
+      return describe("selected list", function() {
         var list, selected;
         list = null;
         selected = null;
@@ -214,28 +214,28 @@
           list = selector.$('.selected');
           return selected = selector.selected;
         });
-        return it("should remove selected items from the autocomplete dropdown", function() {
+        it("should remove selected items from the autocomplete dropdown", function() {
           var first_friend;
           first_friend = selector.friends.models[0];
           selected.collection.add(first_friend);
           selector.search.set_query(first_friend.attributes.name);
           return expect(selector.autocomplete.collection.models).not.toContain(first_friend);
         });
-      });
-      return describe("when I press remove a selected friend", function() {
-        beforeEach(function() {
+        return describe("when I press remove a selected friend", function() {
           var first_friend;
-          first_friend = selector.friends.models[0];
-          return selected.add_model(first_friend);
-        });
-        it("the friend should not appear in the selected friends", function() {
-          selected.items[0].remove();
-          return expect(selected.collection.models).toNotContain(first_friend);
-        });
-        return it("the friend should appear in the autocomplete when I search for        their name", function() {
-          selected.items[0].remove();
-          selector.search.set_query(first_friend.attributes.name);
-          return expect(selector.autocomplete.collection.models).toContain(first_friend);
+          first_friend = null;
+          beforeEach(function() {
+            first_friend = selector.friends.models[0];
+            selected.collection.add(first_friend);
+            return selected.items[0].remove();
+          });
+          it("the friend should not appear in the selected friends", function() {
+            return expect(selected.collection.models).toNotContain(first_friend);
+          });
+          return it("the friend should appear in the autocomplete when I search for        their name", function() {
+            selector.search.set_query(first_friend.attributes.name);
+            return expect(selector.autocomplete.collection.models).toContain(first_friend);
+          });
         });
       });
     });
